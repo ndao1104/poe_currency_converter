28a675a6864eb34d228c3d42ceb1c47b573ce50b
ndao1 <ndao1104@gmail.com>
2026-08-30 14:54:54 -0400

    Use live divine to chaos rate

--- app.py
+++ app.py
@@ -1,10 +1,31 @@
-from flask import Flask, render_template
+import requests
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+@app.route("/api/leagues")
+def get_leagues():
+    response = requests.get("https://poe.ninja/poe1/api/economy/leagues")
+    data = response.json()
+    return jsonify(data)
+
+@app.route("/api/currency")
+def get_currency():
+    league = "Allflame"
+    response = requests.get(f"https://poe.ninja/poe1/api/economy/stash/current/currency/overview?league={league}&type=Currency")
+    data = response.json()
+    return jsonify(data)
+    
+@app.route("/api/cards")
+def get_cards():
+    league = "Allflame"
+    response = requests.get(f"https://poe.ninja/poe1/api/economy/exchange/current/overview?league={league}&type=DivinationCard")
+    data = response.json()
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,7 +1,61 @@
 const convertButton = document.getElementById("convert-button");
+let divineLine;
+let theFortunate;
+let divineBeauty;
+let theSephirot;
 
-convertButton.addEventListener("click", function() {
-    const chao = Number(document.getElementById("divine-amount").value) * 200;
-    document.getElementById("result").textContent = `${chao} Chaos Orbs`;
+fetch("/api/leagues")
+  .then((response) => response.json())
+  .then((data) => {
+    console.log(data);
+  });
+
+fetch("/api/currency")
+  .then((response) => response.json())
+  .then((data) => {
+    divineLine = data.lines.find(
+      (line) => line.currencyTypeName === "Divine Orb",
+    );
+    console.log(divineLine);
+  });
+
+const knownCards = [
+  {
+    id: "the-fortunate",
+    name: "The Fortunate",
+    stackSize: 12,
+    rewardDivines: 2,
+  },
+  {
+    id: "divine-beauty",
+    name: "Divine Beauty",
+    stackSize: 12,
+    rewardDivines: 7,
+  },
+  {
+    id: "the-sephirot",
+    name: "The Sephirot",
+    stackSize: 11,
+    rewardDivines: 10,
+  },
+];
+
+fetch("api/cards")
+  .then((response) => response.json())
+  .then((data) => {
+    theFortunate = data.lines.find((line) => line.id === "the-fortunate");
+    divineBeauty = data.lines.find((line) => line.id === "divine-beauty");
+    theSephirot = data.lines.find((line) => line.id === "the-sephirot");
+
+    array.forEach((card) => {
+      const priceLine = data.lines.find((line) => line.id === card.id);
+      console.log(card.id, priceLine);
+    });
+  });
+
+convertButton.addEventListener("click", function () {
+  const chao =
+    Number(document.getElementById("divine-amount").value) *
+    divineLine.chaosEquivalent;
+  document.getElementById("result").textContent = `${chao} Chaos Orbs`;
 });
-
